770ade33bdefa93e3f2074c65cb2b29e80f541e6
lucascbeyeler <lucas.costab@outlook.com>
2017-07-09 17:06:04 -0300

    Rewriting playbook for multiserver purpose

---
+++ files/config.php
@@ -0,0 +1,25 @@
+<?php
+
+# mysql:host=xx;dbname=yyy
+#
+# pgsql:host=xx;dbname=yyy
+#
+# sqlite:////full/unix/path/to/file.db?mode=0666
+#
+#$DB_DSN="sqlite:////tmp/cluebringer.sqlite";
+#$DB_DSN="mysql:host=localhost;dbname=cluebringer";
+$DB_DSN="sqlite:/opt/zimbra/data/cbpolicyd/db/cbpolicyd.sqlitedb";
+$DB_USER="root";
+#$DB_PASS="";
+$DB_TABLE_PREFIX="";
+
+
+#
+# THE BELOW SECTION IS UNSUPPORTED AND MEANT FOR THE ORIGINAL SPONSOR OF V2
+#
+
+#$DB_POSTFIX_DSN="mysql:host=localhost;dbname=postfix";
+#$DB_POSTFIX_USER="root";
+#$DB_POSTFIX_PASS="";
+
+?>
